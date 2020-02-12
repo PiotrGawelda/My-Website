bcec7877053ba1f9654a01be020b7d7c62d9c57c
Piotr Gawełda <piotr.gawelda@gmail.com>
2020-02-12 14:16:58 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,24 @@
 import React from "react"
 import styled from "styled-components"
 import Container from "./container";
+import Preloader from "./preloader";
 
 const StyledHeader = styled.header`
     background-color: #000000;
-    color: #ffffff;
     height: 100vh;
 `;
 
-const Header = () => (
-    <StyledHeader>
-        <Container>
-            <h1>Hello</h1>
-            <p>Welcome on my website</p>
-        </Container>
-    </StyledHeader>
-)
+const Header = () => {
+
+
+    return (
+        <StyledHeader>
+            <Container>
+                <Preloader/>
+            </Container>
+        </StyledHeader>
+
+    )
+}
 
 export default Header
